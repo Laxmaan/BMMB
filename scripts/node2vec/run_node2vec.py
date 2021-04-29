8dd620a922835dc3b097fb8296aecd48780501d6
Laxmaan <laxmaan15018@cse.ssn.edu.in>
2021-04-29 16:02:22 -0400

    fix

--- scripts/node2vec/run_node2vec.py
+++ scripts/node2vec/run_node2vec.py
@@ -75,7 +75,7 @@ def learn_embeddings(walks,outfile):
     walks = [list(map(str, walk)) for walk in walks]
     model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
     Path(outfile).parent.mkdir(exist_ok=True,parents = True)
-    vectors = np.array(model.wv)
+    vectors = model.wv.vectors_vocab
     print(vectors.shape)
     emb = vectors.mean(axis=0)
     np.savetxt(outfile, emb, delimiter=',')
