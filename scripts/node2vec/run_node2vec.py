69484c291d3a8be6efe2edb7f04b17c4ff1b3e60
Laxmaan <laxmaan15018@cse.ssn.edu.in>
2021-04-29 14:59:28 -0400

    update for roar

--- scripts/node2vec/run_node2vec.py
+++ scripts/node2vec/run_node2vec.py
@@ -3,15 +3,17 @@ from argparse import ArgumentParser
 from joblib import Parallel, delayed
 
 def process_file(args):
-    MAIN_FILE, OUTPUT_ROOT, fpath,dim,walk_len,n_walks = args
+    MAIN_FILE, OUTPUT_ROOT, fpath,dim,walk_len,n_walks,roar = args
     fname_stem = fpath.stem
 
     path = '/'.join(fpath.parent.parts[4:])
 
     outfpath = OUTPUT_ROOT / path / f'{fname_stem}.emb'
 
-    return f'python {MAIN_FILE} --input {fpath} --output {outfpath} --dimensions {dim} --directed --walk-length {walk_len} --num-walks {n_walks}'
-
+    if not roar:
+        return f'python {MAIN_FILE} --input {fpath} --output {outfpath} --dimensions {dim} --directed --walk-length {walk_len} --num-walks {n_walks}'
+    else:
+        return f'activate "BMMB && python {MAIN_FILE} --input {fpath} --output {outfpath} --dimensions {dim} --directed --walk-length {walk_len} --num-walks {n_walks}"'
 if __name__ == '__main__':
     
     parser = ArgumentParser()
@@ -19,6 +21,7 @@ if __name__ == '__main__':
     parser.add_argument("-d",help="embedding dimensions",type=int,default=128)
     parser.add_argument('--walk-length', type=int, default=80, help='Length of walk per source. Default is 80.')
     parser.add_argument('--num-walks', type=int, default=10, help='Number of walks per source. Default is 10.')
+    parser.add_argument('--roar', help='usage on Roar systems', action='store_true')
     args = parser.parse_args()
 
     DATA_ROOT = Path("datasets") / 'processed' / f'{args.k}' / 'node2vec'
@@ -29,7 +32,7 @@ if __name__ == '__main__':
 
     lines = Parallel(n_jobs=-1, verbose=50)(
                                                 delayed(process_file)( (MAIN_FILE, OUTPUT_ROOT, files[i], args.d,
-                                                args.walk_length,args.num_walks) ) for i in range(len(files)) 
+                                                args.walk_length,args.num_walks,args.roar) ) for i in range(len(files)) 
                                             )
     
     with open(f'exec_node2vec_{args.k}.sh','w') as f:
