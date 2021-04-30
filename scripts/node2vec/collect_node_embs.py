d0130785e815c0d54c636a044c2c06bee47e52d1
Laxmaan <laxmaan15018@cse.ssn.edu.in>
2021-04-29 20:59:14 -0400

    n2v to csv

---
+++ scripts/node2vec/collect_node_embs.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+
+if __name__=="__main__":
+    N2V_ROOT = Path('scripts/node2vec/emb')
+    Ks = [4,6,9]
+
+    for k in Ks:
+        DATA_ROOT = N2V_ROOT / f'{k}'
+        files = DATA_ROOT.glob("**/*.emb")
+        out = defaultdict(list)
+        dim = None
+        for f in files:
+            identifier = f.stem
+            label = f"{f.parent.parts[4]}/{f.parent.parts[5]}"
+            arr = np.loadtxt(f,delimiter=',')
+            out[label].append([int(identifier)] + list(arr))
+            if not dim:
+                dim = len(arr)
+        cols = ['idx'] + [f"x_{i}" for i in range(dim)]
+
+        for label,df in out.items():
+            df = pd.DataFrame(df, columns=cols)
+            df = df.sort_values(["idx"])
+
+            output_path = DATA_ROOT / Path(label+".csv")
+
+            Path(output_path).parent.mkdir(exist_ok=True,parents=True)
+            df.to_csv(output_path, index=None)
+        
+
